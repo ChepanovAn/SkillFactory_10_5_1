a7c2cf6603336c48f460395b1c6bc89962abc307
Анатолий <Chepanov199287@gmail.com>
2023-05-11 20:18:33 +0300

    Добавьте файлы проекта.

---
+++ ConsoleProcessor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillFactory_10_5_1
+{
+    class ConsoleProcessor : ISum
+    {
+        public void Sum(int numberOne, int numberTwo)
+        {
+            int result = numberOne + numberTwo;
+            Console.WriteLine($"Результат: {result}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillFactory_10_5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Введите первое число");
+
+                    int numberOne = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Введите второе число");
+
+                    int numberTwo = int.Parse(Console.ReadLine());
+
+                    ConsoleProcessor consoleProcessor = new ConsoleProcessor();
+
+                    ((ISum)consoleProcessor).Sum(numberOne, numberTwo);
+                }
+                catch
+                {
+                    Console.WriteLine("Возникло исключение");
+                }
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
